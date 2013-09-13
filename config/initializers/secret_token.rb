e8b80d631e3729cb09055a5e07948719a39da5cc
Ignacia Orosteguis <ignaciaorosteguis@pc-215-142-160-190.cm.vtr.net>
2013-09-12 21:08:44 -0300

    Primero test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Naxita::Application.config.secret_key_base = '7e59d7dd2b69141e277cdf03f168f4c4628390c4c189342b845f0c96c6fbc28e5397015021453dbdab3873eef491bdc9296a2f0e649abee8ff3aabb26f0336f7'
